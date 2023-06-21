6cb990d5a3eafdf56a4bbceb7f62eddc3c64d16a
Raymond Liu <wfliu66@hotmail.com>
2023-06-21 23:46:38 +0800

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I was changed. Bye for Now! Bean!v1
+          Edit <code>src/App.js</code> and save to reload. I was changed. Bye for Now! Bean!v2
         </p>
         <a
           className="App-link"
